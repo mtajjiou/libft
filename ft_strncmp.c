4025eb5e871702fdd45e3d5188daa844176a118f
Mohamed Tajjiou <mtajjiou@42.us.org>
2018-03-10 18:55:47 -0800

    commit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,9 +17,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	if (n == 0)
 		return (0);
-	while (s1[i] != '\0' && s1[i] == s2[i] && i < n - 1)
+	while (s1[i] != '\0' && s1[i] == s2[i] && i < (int)n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
